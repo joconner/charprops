51485a5930da227c7978991011220a0e69f0aba9
John O'Conner <jsoconner@gmail.com>
2016-05-16 17:54:36 -0700

    Refer to the template correctly with .json suffix.

--- src/deploy/scripts/createnetwork.sh
+++ src/deploy/scripts/createnetwork.sh
@@ -4,7 +4,7 @@ stackstatus=""
 attempts=1
 
 STACKNAME="stack-charprop-network"
-STACKTEMPLATE="../cloudformation/network.template"
+STACKTEMPLATE="../cloudformation/network.json"
 ATTEMPT_LIMIT=10
 
 aws cloudformation create-stack --stack-name $STACKNAME --template-body file://$STACKTEMPLATE
